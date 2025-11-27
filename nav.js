cb033d0d917860858aa5821e915fca0290f2ce4b
R3DCI <salehredapf@gmail.com>
2025-11-27 15:47:53 +0000

    Update nav.js

--- nav.js
+++ nav.js
@@ -3,19 +3,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const closeBtn = document.querySelector(".close-menu");
     const nav = document.querySelector("nav");
 
-    // OUVRIR LE MENU
     toggle.addEventListener("click", () => {
         nav.classList.add("active");
         closeBtn.classList.add("active");
     });
 
-    // FERMER LE MENU AVEC X
     closeBtn.addEventListener("click", () => {
         nav.classList.remove("active");
         closeBtn.classList.remove("active");
     });
 
-    // FERMER APRÈS CLIC SUR UN LIEN
     nav.querySelectorAll("a").forEach(link => {
         link.addEventListener("click", () => {
             nav.classList.remove("active");
