ff091796c62c4d50bcbc2f132587217b77c04637
R3DCI <salehredapf@gmail.com>
2025-11-27 11:37:06 +0000

    Update script.js

--- script.js
+++ script.js
@@ -16,14 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
             const adresse = data.get("adresse");
 
             const message =
-                `Demande de bouquet sur mesure:%0A` +
-                `Budget : ${budget}%0A` +
-                `Couleurs : ${couleurs}%0A` +
-                `Style : ${style}%0A` +
-                `Occasion : ${occasion}%0A` +
-                `Date : ${date}%0A` +
-                `Heure : ${heure}%0A` +
-                `Adresse : ${adresse || "Non précisé"}`;
+                `Bonjour, je souhaite une création florale sur-mesure.%0A%0A` +
+                `▪️ Budget : ${budget}%0A` +
+                `▪️ Palette : ${couleurs}%0A` +
+                `▪️ Style : ${style}%0A` +
+                `▪️ Occasion : ${occasion}%0A` +
+                `▪️ Date : ${date}%0A` +
+                `▪️ Heure : ${heure}%0A` +
+                `▪️ Adresse : ${adresse || "Non précisé"}%0A%0A` +
+                `Merci !`;
 
             window.location.href = `https://wa.me/2250700000000?text=${message}`;
         });
