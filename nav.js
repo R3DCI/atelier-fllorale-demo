39e3fb5262a0bdd03f2d4cfed28a8b01487d8b99
R3DCI <salehredapf@gmail.com>
2025-11-27 11:57:56 +0000

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,8 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.querySelector(".menu-toggle");
+    const closeBtn = document.querySelector(".close-menu");
     const nav = document.querySelector("nav");
 
+    // Ouvrir
     toggle.addEventListener("click", () => {
-        nav.classList.toggle("active");
+        nav.classList.add("active");
+    });
+
+    // Fermer
+    closeBtn.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+
+    // Fermer en cliquant un lien
+    nav.querySelectorAll("a").forEach(link => {
+        link.addEventListener("click", () => {
+            nav.classList.remove("active");
+        });
     });
 });
